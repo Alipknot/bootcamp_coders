17e810f5bc4694608b69e44497677af5bde4f311
¨¨Nicolas¨ <com16nico@gmail.com>
2022-09-02 20:48:35 -0400

    funcionalidad 1 completada

--- app/models/estudiante.rb
+++ app/models/estudiante.rb
@@ -1,4 +1,15 @@
 class Estudiante < ApplicationRecord
   belongs_to :comuna
   belongs_to :curso
+
+  def self.search(region, codigo)
+    query = Estudiante.all
+    if region.present?
+      query = query.joins(:comuna).where(comuna: {region_id: region})
+    end 
+    if codigo.present?
+      query = query.where(curso_id: codigo)
+    end    
+    query
+  end  
 end
